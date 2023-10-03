ecf6297fa891e1192804fca52af1aa9f77f89247
trishadubey39 <trisha.d@smytten.com>
2023-10-03 23:37:35 +0530

    news-view

--- lib/home_page/home_page_view.dart
+++ lib/home_page/home_page_view.dart
@@ -1,27 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../network/NetworkController.dart';
 import 'home_page_logic.dart';
 
-class HomePagePage extends StatefulWidget {
-  const HomePagePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePagePage> createState() => _HomePagePageState();
-}
-
-class _HomePagePageState extends State<HomePagePage> {
-  final logic = Get.find<HomePageLogic>();
-  final state = Get.find<HomePageLogic>().state;
+class HomePagePage extends StatelessWidget {
+  final NewsController newsController = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News App'),
+      ),
+      body: Obx(
+            () =>
+            ListView.builder(
+              itemCount: newsController.newsList.length,
+              itemBuilder: (context, index) {
+                final news = newsController.newsList[index];
+                return ListTile(
+                  title: Text(news.title.toString()),
+                  subtitle: Text(news.description.toString()),
+                  onTap: () {
+                    // Handle article click
+                  },
+                );
+              },
+            ),
+      ),
+    );
   }
-
-  @override
-  void dispose() {
-    Get.delete<HomePageLogic>();
-    super.dispose();
-  }
+}
+//   const HomePagePage({Key? key}) : super(key: key);
+//
+//   @override
+//   State<HomePagePage> createState() => _HomePagePageState();
+// }
+//
+// class _HomePagePageState extends State<HomePagePage> {
+//   final logic = Get.find<HomePageLogic>();
+//   final state = Get.find<HomePageLogic>().state;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+//
+//   @override
+//   void dispose() {
+//     Get.delete<HomePageLogic>();
+//     super.dispose();
+//   }
 }
